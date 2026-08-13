955cb0b8e6a1054926320941f4b51c0b92a2a6a3
Jax-rgb <jacquescarigma@gmail.com>
2026-08-13 23:02:57 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const envelope = document.getElementById('envelope');
-
-  // Toggle open/close on click or tap
-  envelope.addEventListener('click', () => {
-    envelope.classList.toggle('open');
-  });
-});
